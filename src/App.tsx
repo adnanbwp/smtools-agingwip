04e8abe35b682d60d3c22df24afa7220837cb5b4
Adnan Ali <adnanbwp@gmail.com>
2024-07-01 20:30:00 +1000

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>Welcome to the Aging Work In Progress Chart app!</p>
+    </div>
+  )
+}
+
+export default App
